6abeb5b774cf9e2d18ae069fda45bc183ef9b42a
a3amat <admin@school4.org>
2023-01-29 19:38:39 +0500

    update start.sh, adding parameter

--- start.sh
+++ start.sh
@@ -4,18 +4,25 @@ python3 -m venv env
 source env/bin/activate
 pip install -U pip
 pip install django
-django-admin startproject note
+django-admin startproject $1
 
-sleep 3
+name=$1
 
-cd note
+cd ${name}
 git clone git@github.com:a3amat/notes.git
-mv notes/* ./
+cp -r notes/. ./
 rm -rf notes
+
+mv ${name} config
+sed -i "s/${name}/config/g" config/asgi.py
+sed -i "s/${name}/config/g" config/wsgi.py
+sed -i "s/${name}/config/g" config/settings.py
+sed -i "s/${name}/config/g" config/settings.py
+sed -i "s/${name}/config/g" manage.py
+
 
 echo 'Можете проверить запущенный Django'
 
 ./manage.py runserver
-
 
 echo 'Exit'
