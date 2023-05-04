b7f6071f36c33aae4e0a90f492a027c40d04733e
Azamat <AYulmukhametov@egartech.ru>
2023-05-04 11:11:49 +0300

    исправлено отображение наименования статьи

--- article/models.py
+++ article/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 
 class Article(models.Model):
+
+    class Meta:
+        db_table = 'article'
+        
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
