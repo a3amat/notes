73b0d3eb9aacef4d812933ff68c9b8ba1331f81b
Azamat <AYulmukhametov@egartech.ru>
2023-06-02 23:28:57 +0300

    Недоработан импорт и экспорт данных

--- article/admin.py
+++ article/admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
-
 from .models import Article
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ExportActionModelAdmin
+
+
+# класс обработки данных
+class ArticleResource(resources.ModelResource):
+
+    class Meta:
+        model = Article
+
+# вывод данных на странице
+class ArticleAdmin(ImportExportModelAdmin):
+    resource_classes = [ArticleResource]
+
+
+class ArticleAdmin(ExportActionModelAdmin):
+    pass
+
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'author', 'create_date', 'update_date']
-    list_filter = ['create_date', 'update_date']
-    search_fields = ['name', 'author', 'create_date', 'update_date']
-
+     list_display = ['__str__', 'author', 'create_date', 'update_date']
+     list_filter = ['create_date', 'update_date']
+     search_fields = ['name', 'author', 'create_date', 'update_date']
 
 
 
 admin.site.register(Article, ArticleAdmin)
-
